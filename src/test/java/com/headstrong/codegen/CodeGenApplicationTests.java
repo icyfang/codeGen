a6fc10ee7b81db4a0699a6372666b50b60819028
icyfang <chuche1993@qq.com>
2021-08-12 22:08:26 +0800

    init project

---
+++ src/test/java/com/headstrong/codegen/CodeGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.headstrong.codegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeGenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
